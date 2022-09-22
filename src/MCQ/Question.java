2da764c261dac7062e0b2b0139bd677663dd8de9
Murali.r <iammurali.mng@gmail.com>
2022-09-22 13:50:29 +0530

    update

--- src/MCQ/Question.java
+++ src/MCQ/Question.java
@@ -7,8 +7,18 @@ public class Question {
 	public static void main(String[] args) {
 		JFrame f=new JFrame("MCQ");//creating instance of JFrame  
         
-		JButton b=new JButton("MCQ");//creating instance of JButton  
-		b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
+		JButton b=new JButton("SUBMIT");//creating instance of JButton  
+		b.setBounds(90,150,100, 35);//x axis, y axis, width, height  
+		
+		JButton c=new JButton("CANCEL");//creating instance of JButton  
+		c.setBounds(200,150,100, 35);//x axis, y axis, width, height 
+		
+		JTextField t1=new JTextField();  
+		t1.setBounds(30,10, 250,30);  
+		
+		f.add(t1);
+		
+		f.add(c);
 		          
 		f.add(b);//adding button in JFrame  
 		          
